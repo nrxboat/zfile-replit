ae8d4692f70f214badbcb3f491f2f0fd2e2cb4e7
nrxboat <69232821+nrxboat@users.noreply.github.com>
2023-03-13 16:30:36 +0000

    现在创建完项目后可以修改项目名称了

--- bash/update.sh
+++ bash/update.sh
@@ -1,9 +1,9 @@
 bash ./zfile/bin/stop.sh     #停止          
-cp ./zfile/WEB-INF/classes/allplcation.propperties /home/runner/zfile #备份配置文件
+cp ./zfile/WEB-INF/classes/allplcation.propperties /home/runner/${REPL_SLUG} #备份配置文件
 
 rm -rf ./zfile   #删除运行目录
 
-export ZFILE_INSTALL_PATH=/home/runner/zfile-replit/zfile     #设置安装目录                        
+export ZFILE_INSTALL_PATH=/home/runner/${REPL_SLUG}/zfile     #设置安装目录                        
 
 mkdir -p $ZFILE_INSTALL_PATH && cd $ZFILE_INSTALL_PATH   #创建安装目录并进入   
 
@@ -11,8 +11,8 @@ wget --no-check-certificate https://c.jun6.net/ZFILE/zfile-release.war      #下
 
 unzip zfile-release.war && rm -rf zfile-release.war   #解压        
 
-mv /home/runner/zfile-replit/application.propperties /home/runner/zfile/zfile/WEB-INF/classes/     #恢复配置文件
+mv /home/runner/${REPL_SLUG}/application.propperties /home/runner/${REPL_SLUG}/zfile/WEB-INF/classes/     #恢复配置文件
 
 chmod +x $ZFILE_INSTALL_PATH/bin/*.sh   #授权启动停止脚本  
 
-#bash /home/runner/zfile-replit/main.sh #启动脚本
+#bash /home/runner/${REPL_SLUG}/main.sh #启动脚本
